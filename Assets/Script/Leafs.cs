8f85e9eac74aa32625441421e96cf63c613065d1
raybac38 <37730900+raybac38@users.noreply.github.com>
2024-02-08 18:31:52 +0100

    Leafs MAJ and documentation

--- Assets/Script/Leafs.cs
+++ Assets/Script/Leafs.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+using System;
 using UnityEngine;
 
 public class Leafs : MonoBehaviour
@@ -6,71 +6,111 @@ public class Leafs : MonoBehaviour
     private MeshFilter meshFilter;
     private Mesh mesh;
 
-    private double density = 1;
+    private float density = 1;
     private int densityPowerTen = 1;
 
-    private double totalSurface;
-    private double[] surfaceTriangle;
+    private float totalSurface;
+    private float[] surfaceTriangle;
 
-    private void Awake() 
+    private Vector3[] points;
+
+    private Vector3[] vertices;
+    int[] triangles;
+
+    int nbPoints = 0;
+
+    private void Awake()
     {
-    
         meshFilter = GetComponent<MeshFilter>();
-        if(meshFilter == null)
+        if (meshFilter == null)
         {
             this.enabled = false;
             Debug.LogError("MeshFilter not found");
             return;
         }
-        mesh = meshFilter.mesh;
-        CalculateTotalAndTriangleSurface();
 
+        mesh = meshFilter.mesh;
+        triangles = mesh.triangles;
+        vertices = mesh.vertices;
+
+        CalculateTotalAndTriangleSurface();
     }
 
     private void CalculatePointsOnSurface(int startingIndex)
     {
-        
+        nbPoints = CalculateTotalPoints();
+        points = new Vector3[nbPoints];
+        Vector3 newPoint;
+
+        for (int index = 0; index < nbPoints; index++)
+        {
+            int indexTriangle = FindRandomTriangle();
+            newPoint = GenerateRandomPointOnTriangle(indexTriangle);
+            points[index] = newPoint;
+        }
     }
 
 
-    private Vector3 GenerateRandomPointOnTriangle(int indexTriangle)
+    private int FindRandomTriangle()
     {
 
+        if (surfaceTriangle.Length == 0)
+        {
+            throw new Exception("Triangle Array Empty");
+
+        }
+        float randomSurface = Random.Range(0f, totalSurface);
+        int indexTriangle = -1;
+        float area = 0;
+        int maxIndexTriangle = surfaceTriangle.Length;
+        while (area <= randomSurface && maxIndexTriangle != indexTriangle)
+        {
+            indexTriangle++;
+            area += surfaceTriangle[indexTriangle];
+        }
+        return indexTriangle;
+    }
+
+    private Vector3 GenerateRandomPointOnTriangle(int indexTriangle)
+    {
+        Vector3 u = verticies[triangle[indexTriangle]];
+        Vector3 v = verticies[triangle[indexTriangle + 1]];
+        Vector3 w = verticies[triangle[indexTriangle + 2]];
+
+        Vector3 scalaire = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)).normalized;
+
+        return (u * scalaire.x) + (v * scalaire.y) + (w * scalaire.z);
     }
 
     private void CalculateTotalAndTriangleSurface()
     {
-        var triangles = mesh.triangles;
-        var vertices = mesh.vertices;
+
         int surfaceNumber = triangles.Length / 3;
 
-        surfaceTriangle = new double[surfaceNumber];
+        surfaceTriangle = new float[surfaceNumber];
 
         foreach (var triangleIndex in Enumerable.Range(0, surfaceNumber))
         {
-            Vector3 triangleUn = vertices[triangles[triangleIndex * 3]]; 
-            Vector3 triangleDeux = vertices[triangles[triangleIndex * 3 + 1]]; 
-            Vector3 triangleTrois = vertices[triangles[triangleIndex * 3 + 2]]; 
-            
-            double surface = GetTriangleArea(triangleUn, triangleDeux, triangleTrois);
+            Vector3 triangleUn = vertices[triangles[triangleIndex * 3]];
+            Vector3 triangleDeux = vertices[triangles[triangleIndex * 3 + 1]];
+            Vector3 triangleTrois = vertices[triangles[triangleIndex * 3 + 2]];
+
+            float surface = GetTriangleArea(triangleUn, triangleDeux, triangleTrois);
             totalSurface += surface;
             surfaceTriangle[triangleIndex] = surface;
         }
-
-        Debug.Log("Total surface: " + totalSurface);
-        Debug.Log("Total points: " + CalculateTotalPoints());
     }
 
     private int CalculateTotalPoints()
     {
-        double result = density * totalSurface * Mathf.Pow(10, densityPowerTen);
-        return Mathf.RoundToInt((float)result);
-    }   
+        float result = (float)(density * totalSurface * Mathf.Pow(10, densityPowerTen));
+        return Mathf.RoundToInt(result);
+    }
 
-    private double GetTriangleArea(Vector3 a, Vector3 b, Vector3 c)
+    private float GetTriangleArea(Vector3 a, Vector3 b, Vector3 c)
     {
         Vector3 ab = b - a;
         Vector3 ac = c - a;
-        return 0.5 * Vector3.Cross(ab, ac).magnitude;
+        return 0.5f * Vector3.Cross(ab, ac).magnitude;
     }
 }
